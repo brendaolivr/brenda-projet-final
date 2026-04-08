00b0e2f0ce6725019f262193450120b81a27aa83
Brenda Oliveira <105824706+brendaolivr@users.noreply.github.com>
2026-04-08 10:11:34 +0000

    mise à jour du code

--- Sources/App/Database.swift
+++ Sources/App/Database.swift
@@ -1,7 +1,7 @@
 import SQLite
 import Foundation
 
-// Connection uses an internal serial queue, so it is safe to mark Sendable.
+
 extension Connection: @unchecked @retroactive Sendable {}
 
 struct Database {
